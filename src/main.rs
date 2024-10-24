f9daf85d4049efb09f1a4481fa92ad3cc76fc274
Jakub <pecet3107@gmail.com>
2024-10-25 00:42:44 +0200

    okli

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use bevy::prelude::*;
+use rand::Rng;
 fn main() {
     let mut app = App::new();
     app.add_plugins(DefaultPlugins);
-    app.add_systems(Startup, (spawn_camera, spawn_players));
-    app.add_systems(Update, move_paddle);
+    app.add_systems(Startup, (spawn_camera, spawn_players, spawn_ball));
+    app.add_systems(Update, (move_paddle, move_ball, ball_collision));
     app.run();
     println!("Hello, world!");
 }
@@ -66,9 +67,58 @@ fn move_paddle(
     for (mut pos, settings) in &mut paddles {
         if input.pressed(settings.move_up) {
             pos.translation.y += 100. * time.delta_seconds();
+            pos.translation.y = pos.translation.y.clamp(-250. + 75., 250. - 75.)
         }
         if input.pressed(settings.move_down) {
             pos.translation.y -= 100. * time.delta_seconds();
+            pos.translation.y = pos.translation.y.clamp(-250. + 75., 250. - 75.)
+        }
+    }
+}
+#[derive(Component)]
+struct Ball(Vec2);
+
+fn spawn_ball(mut commands: Commands) {
+    commands.spawn((
+        SpriteBundle {
+            transform: Transform::from_translation(Vec3::new(0., 0., 1.)),
+            sprite: Sprite {
+                color: Color::WHITE,
+                custom_size: Some(Vec2::new(10., 10.)),
+                ..Default::default()
+            },
+            ..Default::default()
+        },
+        Ball(Vec2::new(-100., 0.)),
+    ));
+}
+
+fn move_ball(mut balls: Query<(&mut Transform, &Ball)>, time: Res<Time>) {
+    for (mut pos, ball) in &mut balls {
+        pos.translation += ball.0.extend(0.) * time.delta_seconds()
+    }
+}
+
+const BWIDTH: f32 = 25.;
+const PWIDTH: f32 = 10.;
+const PHIGHT: f32 = 150.;
+fn ball_collision(
+    mut balls: Query<(&Transform, &mut Ball)>,
+    paddles: Query<&Transform, With<Paddle>>,
+) {
+    for (ball, mut velocity) in &mut balls {
+        if ball.translation.y.abs() + BWIDTH / 2. > 250. {
+            velocity.0.y *= -1.;
+        }
+        for paddle in &paddles {
+            if ball.translation.x - BWIDTH / 2. < paddle.translation.x + PWIDTH / 2.
+                && ball.translation.y - BWIDTH / 2. < paddle.translation.y + PHIGHT / 2.
+                && ball.translation.x + BWIDTH / 2. > paddle.translation.x - PWIDTH / 2.
+                && ball.translation.y + BWIDTH / 2. > paddle.translation.y - PHIGHT / 2.
+            {
+                velocity.0 *= -1.;
+                velocity.0.y = rand::thread_rng().gen::<f32>() * 100.;
+            }
         }
     }
 }
